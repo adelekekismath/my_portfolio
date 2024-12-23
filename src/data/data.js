52cad5fd524d54ed38837a196cf5e95a93f507a8
adelekekismath9 <adelekekismath9@gmail.com>
2024-12-23 12:56:03 +0100

    feat: add new blog entries with localization support for English and French

--- src/data/data.js
+++ src/data/data.js
@@ -1,18 +1,18 @@
 export default [{
         username: "Kismath ADELEKE",
         date: "2024.20.12",
-        title: "Derrière l'écran : mes échecs, mes leçons, et ma passion du code ",
+        title: "blogs.items[0].title",
         image: "https://miro.medium.com/v2/resize:fit:1400/0*aVG0mK8Xf5iIJqmI.jpeg",
         link: "https://medium.com/@adelekekismath9/derri%C3%A8re-l%C3%A9cran-mes-%C3%A9checs-mes-le%C3%A7ons-et-ma-passion-du-code-47b4597b34d5",
-        intro: "Un parcours authentique et sincère d'une développeuse, entre apprentissage, défis techniques, et résilience face aux échecs. Découvrez les leçons tirées d'un voyage dans le monde du code.",
+        intro: "blogs.items[0].description"
     },
 
     {
         "username": "Kismath ADELEKE",
-        "date": "2024.20.12",
-        "title": "Ma bataille entre le frontend et le backend : Et si on arrêtait de choisir ?",
+        "date": "2024.22.12",
+        "title": "blogs.items[1].title",
         "image": "https://media.geeksforgeeks.org/wp-content/uploads/20190712131938/front-end-vs-back-end.png",
         "link": "https://medium.com/@adelekekismath9/ma-bataille-entre-le-frontend-et-le-backend-et-si-on-arr%C3%AAtait-de-choisir-e1311f536862",
-        "intro": "Découvrez comment j'ai appris à aimer le frontend et le backend, et pourquoi il est temps de mettre fin à la guerre entre les deux."
+        "intro": "blogs.items[1].description"
     }
   ];
--- src/discover/BlogsPage.vue
+++ src/discover/BlogsPage.vue
@@ -11,9 +11,9 @@
             </a>
             <div class="py-5">
                 <a href="#">
-                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ blog[i].title }}</h5>
+                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $t(`${blog[i].title}`) }}</h5>
                 </a>
-                <p class="mb-3  text-gray-700 dark:text-gray-400">{{ blog[i].intro }}</p>
+                <p class="mb-3  text-gray-700 dark:text-gray-400">{{ $t(`${blog[i].intro}`) }}</p>
                 <a :href="blog[i].link" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
