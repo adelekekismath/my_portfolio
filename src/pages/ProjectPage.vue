4cfa830175c18bf92ee5b44b36bb96eae6ff12ab
adelekekismath9 <adelekekismath9@gmail.com>
2025-03-12 15:57:05 +0100

    feat: enhance ProjectPage layout with improved styling and responsive design; update project display and technology section

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,107 +1,111 @@
 <template>
-    <div class="page-section md:pt-12 md:my-[76px] mb-[130px] md:mb-[72px]">
-        <div class="container">
-            <div class="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
-                <div v-for="(project, i) in projects" :key="i"
-                    class="p-3 pb-0 bg-dirty-white my-2 border border-gray-200 rounded-lg shadow dark:bg-gray-800">
-                    <div class="flex flex-col space-y-5 h-full bg-dirty-white rounded-md cursor-pointer">
-                        <div class="flex items-center justify-center h-3/4 w-full rounded-t-lg bg-dirty-white">
-                            <img class="h-full w-full" :src="project.image" />
-                        </div>
+  <section class="page-section md:pt-12 md:my-20 mb-32 md:mb-18">
+    <div class="container">
+      <div class="grid sm:grid-cols-2 md:grid-cols-2 gap-6">
+        <div
+        v-for="(project, index) in projects"
+        :key="index"
+        class=" card p-4 bg-dirty-white transition-all duration-300 rounded-lg  dark:bg-gray-800 transform hover:-translate-y-1"
+        >
 
-                        <div class="flex flex-col">
-                            <div class="text-lg font-medium ml-3 mb-2">
-                                <a>{{ $t(`${project.name}`) }}</a>
-                            </div>
-                            <div class="text-base text-gray-700 ml-3 mr-3 mb-2">
-                                {{ $t(`${project.content}`) }}
-                            </div>
-
-                            <div class="text-base text-gray-700 ml-3 mr-3 my-2">
-                                <strong>{{ $t('projectsPage.time') }}:</strong> {{ $t(`${project.time}`) }}
-                            </div>
-                            <div class="text-base text-gray-700 ml-3 mr-3 my-2">
-                                <strong>{{ $t('projectsPage.whatDidILearn') }} :</strong> {{ $t(`${project.learn}`) }}
-                            </div>
-
-                            <div class="flex flex-row items-start mt-8 mb-2 flex-wrap gap-3 ml-2">
-                                <img v-for="(tech, index) in project.technologies" :key="index" :src="tech" />
-                            </div>
-                        </div>
-                        <div class="my-2">
-                            <a v-if="project.live" :href="project.live" target="_blank"
-                                class="btn p-2 hover:bg-indigo-100 hover:font-medium">
-                                {{ $t("projectsPage.liveDemo") }}
-                                <span class="mai-chevron-forward text-sm"></span>
-                            </a>
-
-                            <a v-if="project.code" :href="project.code" target="_blank"
-                                class="btn p-2 hover:bg-indigo-100 hover:font-medium">
-                                {{ $t("projectsPage.sourceCode") }}
-                                <span class="mai-chevron-forward text-sm"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          <div class="flex flex-col space-y-5 h-full cursor-pointer">
+            <div
+              class="flex items-center justify-center h-full w-full overflow-hidden rounded-t-lg bg-dirty-white"
+            >
+              <img
+                class="h-full w-full object-cover"
+                :src="project.image"
+                :alt="project.name"
+              />
             </div>
+
+            <div class="px-3 flex flex-col justify-between  h-full flex-grow">
+                <div>
+                    <h3 class="text-lg font-semibold mb-2">{{ $t(project.name) }}</h3>
+                    <p class="text-gray-700 mb-2">{{ $t(project.content) }}</p>
+
+                    <p class="text-gray-700 my-2">
+                        <strong>{{ $t("projectsPage.time") }}:</strong>
+                        {{ $t(project.time) }}
+                    </p>
+                    <p class="text-gray-700 my-2">
+                        <strong>{{ $t("projectsPage.whatDidILearn") }} :</strong>
+                        {{ $t(project.learn) }}
+                    </p>
+                </div>
+
+                
+              
+                <div class="flex flex-col pt-2 border-t-2 border-indigo-500 justify-self-end   gap-6" >
+                    
+                    <div class="flex flex-wrap gap-3 ">
+                        <img
+                            v-for="(tech, techIndex) in project.technologies"
+                            :key="techIndex"
+                            :src="tech"
+                            :alt="'Tech: ' + tech"
+                        />
+                    </div>
+
+                    <div class="mt-auto flex gap-3">
+                        <a
+                            v-if="project.live"
+                            :href="project.live"
+                            target="_blank"
+                            class="btn p-2 hover:bg-indigo-100 font-medium transition-colors duration-200"
+                            >
+                                {{ $t("projectsPage.liveDemo") }}
+                        </a>
+
+                        <a
+                            v-if="project.code"
+                            :href="project.code"
+                            target="_blank"
+                            class="btn p-2 hover:bg-indigo-100 font-medium transition-colors duration-200"
+                        >
+                            {{ $t("projectsPage.sourceCode") }}
+                        </a>
+                    </div>
+              </div>
+
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </section>
 </template>
 
 <script>
-    import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 
+export default {
+  name: "ProjectPage",
 
-    export default {
-        name: "Project Page",
+  computed: {
+    ...mapState("projects", ["projects"]),
+  },
 
-        computed: {
-            ...mapState('projects', ['projects'])
-        },
+  created() {
+    this.fetchProjects();
+  },
 
-        created() {
-            this.fetchProjects();
-        },
-
-        methods: {
-            ...mapActions('projects', ['fetchProjects'])
-        },
-
-        props: {
-            blog: Array,
-        },
-    };
+  methods: {
+    ...mapActions("projects", ["fetchProjects"]),
+  },
+};
 </script>
 
 <style scoped>
-    .space-on-top {
-        top: 70px;
-    }
+.card:hover {
+    box-shadow: 0 7px 10px #6C55F9; 
+}
 
-    .project-img {
-        border-radius: 15px;
-        float: left;
-    }
 
-    .project-img:hover {
-        transform: scale(1.3);
-        margin: 80px 0 0 0;
-        z-index: 10;
-        transition: transform 0.8s;
-        width: 100%;
-        height: 100%;
-    }
-
-    @media only screen and (max-width: 700px) {
-        .project-img {
-            width: 49.99999%;
-            margin: 6px 0;
-        }
-    }
-
-    @media only screen and (max-width: 500px) {
-        .project-img {
-            width: 100%;
-        }
-    }
+@media (max-width: 700px) {
+  .project-img {
+    width: 100%;
+    margin: 6px 0;
+  }
+}
 </style>
