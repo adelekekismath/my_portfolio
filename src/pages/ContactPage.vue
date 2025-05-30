fc3fee4d6d20c6ce356de460680fb9a5b0781c3f
adelekekismath9 <adelekekismath9@gmail.com>
2025-05-30 18:55:18 +0200

    feat: refactor email sending functionality to use EmailJS directly in ContactPage.vue

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -56,7 +56,6 @@
                             <span class="font-medium">{{ $t('contactPage.form.success[0]') }}</span> {{
                                 $t('contactPage.form.success[1]') }}
                         </v-alert>
-
                     </form>
                 </div>
             </div>
@@ -93,21 +92,25 @@ export default {
     methods: {
 
         sendEmail() {
-            fetch("/.netlify/functions/send-email", {
-                method: "POST",
-                body: JSON.stringify({
-                name: this.form.from_name,
-                email: this.form.from_email,
-                message: this.form.message
-            })})
-            .then((res)=>res.json())
-            .then((data)=>{
-                this.messageSent = true;
+            const SERVICE_ID = process.env.VUE_APP_SERVICE_ID;
+            const TEMPLATE_ID = process.env.VUE_APP_TEMPLATE_ID;
+            const PUBLIC_KEY = process.env.VUE_APP_PUBLIC_KEY;
+
+            if (!SERVICE_ID || !TEMPLATE_ID || !PUBLIC_KEY) {
+                console.error('EmailJS credentials are not set in the environment variables.');
+                return;
+            }
+            this.messageSent = false;
+            emailjs.send(SERVICE_ID, TEMPLATE_ID, this.form, {
+                publicKey: PUBLIC_KEY
             })
-            .catch((error)=>{
-                console.log("Nous avons rencontrés cette erreur", error)
-            })
-            
+                .then((response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                    this.resetForm();
+                    this.messageSent = true;
+                }, (error) => {
+                    console.log('FAILED...', error);
+                });
         },
 
         resetForm() {
